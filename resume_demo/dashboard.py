da1695d0ac41cd87ef3734df1e79c1eededb3e58
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-05-23 19:26:59 +0700

    Add admin-tools-stats feature

---
+++ resume_demo/dashboard.py
@@ -0,0 +1,22 @@
+from admin_tools_stats.modules import DashboardCharts, get_active_graph
+
+# append an app list module
+self.children.append(modules.AppList(
+    _('Dashboard Stats Settings'),
+    models=('admin_tools_stats.*', ),
+))
+
+# Copy following code into your custom dashboard
+# append following code after recent actions module or
+# a link list module for "quick links"
+graph_list = get_active_graph()
+for i in graph_list:
+    kwargs = {}
+    kwargs['require_chart_jscss'] = True
+    kwargs['graph_key'] = i.graph_key
+
+    for key in context['request'].POST:
+        if key.startswith('select_box_'):
+            kwargs[key] = context['request'].POST[key]
+
+    self.children.append(DashboardCharts(**kwargs))
--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_tools_stats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'main',
     'ckeditor',
     'hitcount',
+
+    'django_nvd3',
 ]
 
 MIDDLEWARE = [
